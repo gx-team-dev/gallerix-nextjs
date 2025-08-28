565ed37f4dd927f6409eb0272e4010bf176f8449
Mathias Heikius <mathiasheikius@gmail.com>
2025-08-28 22:26:02 +0200

    added som basic structures and files

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+type HeaderProps = {
+  className?: string;
+  props?: string;
+}
+
+export const Header = async ({className, ...props}: HeaderProps) => {
+
+  return (
+    <header className={cn("flex items-center justify-center p-4 bg-white shadow-md", className)} {...props}>
+      <Link href="/" className="flex items-center">
+        <Image src="/gallerix-logo.svg" alt="Gallerix Logo" width={150} height={50} />
+      </Link>
+    </header>
+  );
+          
+};
