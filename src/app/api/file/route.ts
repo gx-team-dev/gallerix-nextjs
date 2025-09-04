9ba8d0ac2e1f435ebcaf9b2eaf07a3f52bc89c18
Mathias Heikius <mathiasheikius@gmail.com>
2025-09-04 11:23:44 +0200

    ....

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function GET() {
-  const usersPath = path.join(process.cwd(), 'public', 'temp/IMG_4601.JPG');
+  const usersPath = path.join(process.cwd(), '/src/app', '/IMG_4601.JPG');
   const file = fs.readFileSync(usersPath);
   return new Response(file);
 };
